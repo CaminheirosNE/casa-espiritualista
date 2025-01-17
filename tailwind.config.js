1e2d0dc5d5160a495ff2343e59349603b0655bb3
CaminheirosNE <solsaobentodosul@gmail.com>
2025-01-17 15:29:54 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'card': 'hsl(0 0% 100%)',
+        'card-foreground': 'hsl(222.2 84% 4.9%)',
+      }
+    },
   },
   plugins: [],
 }
