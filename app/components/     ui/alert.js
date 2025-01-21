7daf392fdb128acacdd98cab4073dd0d8f2b8a38
CaminheirosNE <solsaobentodosul@gmail.com>
2025-01-21 11:41:36 -0300

    Update alert.js

--- app/components/     ui/alert.js
+++ app/components/     ui/alert.js
@@ -1,32 +1,25 @@
-import * as React from "react";
-import { cva } from "class-variance-authority";
-import { cn } from "@/lib/utils";
+"use client"
 
-const alertVariants = cva(
-  "relative w-full rounded-lg border px-4 py-3 text-sm [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground [&>svg~*]:pl-7",
-  {
-    variants: {
-      variant: {
-        default: "bg-background text-foreground",
-        destructive:
-          "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-    },
-  }
-);
+import * as React from "react"
+import * as AlertDialog from "@radix-ui/react-alert-dialog"
+import { cn } from "@/lib/utils"
 
 const Alert = React.forwardRef(({ className, variant, ...props }, ref) => (
   <div
     ref={ref}
     role="alert"
-    className={cn(alertVariants({ variant }), className)}
+    className={cn(
+      "relative w-full rounded-lg border border-gray-200 p-4",
+      {
+        "bg-gray-50 text-gray-900": variant === "default",
+        "border-red-500 bg-red-50 text-red-900": variant === "destructive",
+      },
+      className
+    )}
     {...props}
   />
-));
-Alert.displayName = "Alert";
+))
+Alert.displayName = "Alert"
 
 const AlertDescription = React.forwardRef(({ className, ...props }, ref) => (
   <div
@@ -34,7 +27,7 @@ const AlertDescription = React.forwardRef(({ className, ...props }, ref) => (
     className={cn("text-sm [&_p]:leading-relaxed", className)}
     {...props}
   />
-));
-AlertDescription.displayName = "AlertDescription";
+))
+AlertDescription.displayName = "AlertDescription"
 
-export { Alert, AlertDescription };
+export { Alert, AlertDescription }
