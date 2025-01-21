aa3f81514883b56edeeda12b5fa76e7bb59769a9
CaminheirosNE <solsaobentodosul@gmail.com>
2025-01-21 19:08:27 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['localhost'],
     remotePatterns: [
@@ -17,7 +14,7 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
+  }
 }
 
 module.exports = nextConfig
