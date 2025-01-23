f531a6ffc819ba476e6b60349108c17763a6c95c
CaminheirosNE <solsaobentodosul@gmail.com>
2025-01-22 22:52:10 -0300

    Atualizar o alert.js

--- app/components/     ui/alert.js
+++ app/components/     ui/alert.js
@@ -18,15 +18,6 @@ const Alert = React.forwardRef(({ className, variant = "default", ...props }, re
 ))
 Alert.displayName = "Alert"
 
-const AlertTitle = React.forwardRef(({ className, ...props }, ref) => (
-  <h5
-    ref={ref}
-    className={cn("mb-1 font-medium leading-none tracking-tight", className)}
-    {...props}
-  />
-))
-AlertTitle.displayName = "AlertTitle"
-
 const AlertDescription = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
@@ -36,4 +27,4 @@ const AlertDescription = React.forwardRef(({ className, ...props }, ref) => (
 ))
 AlertDescription.displayName = "AlertDescription"
 
-export { Alert, AlertTitle, AlertDescription }
+export { Alert, AlertDescription }
