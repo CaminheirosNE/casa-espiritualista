4d920878d09c2ab5b0c6b25be55949694fe88fbe
CaminheirosNE <solsaobentodosul@gmail.com>
2025-01-23 17:04:03 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['localhost'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
